10969f42fa2bbf057e206337ebfe6754a10af8c3
Marvin <maples4000@googlemail.com>
2025-01-28 16:02:13 +0100

    implement tab-panel project

---
+++ src/stories/tab-panel.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from "@storybook/react";
+import TabPanel from "./tab-panel";
+
+const meta: Meta<typeof TabPanel> = {
+    title: "Tab Panel",
+    component: TabPanel,
+};
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Panel: Story = {
+    args: {
+        tabs: [
+            {
+                title: "Tab 1",
+                content: (
+                    <div className="flex flex-col gap-2 items-center">
+                        <p className="text-lg">Header of Test Content 1</p>
+                        <div className="text-white bg-black text-lg p-4 rounded-lg">
+                            Boxed main content of Test Content 1
+                        </div>
+                        <p className="text-lg">Bottom text of Test Content 1</p>
+                    </div>
+                ),
+            },
+        ],
+    },
+};
