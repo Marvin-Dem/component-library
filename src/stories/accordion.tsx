9b2d36c38ad00aab7b33006f6ece47c6a4822c37
Marvin <maples4000@googlemail.com>
2025-01-30 14:55:13 +0100

    first commit

---
+++ src/stories/accordion.tsx
@@ -0,0 +1,8 @@
+export default function Accordion() {
+    return (
+        <div>
+            <div className="text-red-500">Hello World1</div>
+            <div className="text-blue-500">Hello World2</div>
+        </div>
+    );
+}
