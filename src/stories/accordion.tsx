2c60aae11983717ca62b7d7485f5dc0ddb0e1f96
Marvin <maples4000@googlemail.com>
2025-02-11 15:47:37 +0100

    add styling

--- src/stories/accordion.tsx
+++ src/stories/accordion.tsx
@@ -1,4 +1,5 @@
 import { useState, ReactNode } from "react";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
 
 export type AccordionItem = {
     title: string;
@@ -16,14 +17,20 @@ export default function Accordion(props: AccordionProps) {
         <div>
             {props.items.map((item, index) => (
                 <div key={index}>
-                    <button
-                        onClick={() =>
-                            setIsOpen(isOpen === index ? null : index)
-                        }
-                    >
-                        {item.title}
-                    </button>
-                    {isOpen === index && <div>{item.content}</div>}
+                    <div className="text-white bg-black rounded-lg w-1/2 flex justify-between text-xl my-2">
+                        <button
+                            onClick={() =>
+                                setIsOpen(isOpen === index ? null : index)
+                            }
+                            className="mx-2 cursor-pointer"
+                        >
+                            {item.title}
+                        </button>
+                        <IoIosArrowDropdownCircle className="m-2" />
+                    </div>
+                    {isOpen === index && (
+                        <div className="my-2">{item.content}</div>
+                    )}
                 </div>
             ))}
         </div>
